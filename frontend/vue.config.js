aeffeccd20eaba01eef279d9ccb7ab7214076b21
YotinNuntasen <Yotin.n@kkumail.com>
2025-10-07 16:31:54 +0700

    Try To fix Error 500 round 3

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/nbo-matching/' : '/',
-  pluginOptions: {
-    vuetify: {
-      
-    }
-  }
+  // publicPath: process.env.NODE_ENV === 'production' ? '/nbo-matching/' : '/',
+  // pluginOptions: {
+  //   vuetify: {
+
+  //   }
+  // }
 })
